978ba46e08745d0255eefafba06f906008f9e2af
Mikael Levén <mikael.leven@creuna.se>
2014-07-31 19:26:21 +0200

    Version 1.1

--- GameInfoX/GameInfoX.lua
+++ GameInfoX/GameInfoX.lua
@@ -10,12 +10,12 @@
 -------------------------------------------------------------------------------
 
 -- First, we create a namespace for our addon by declaring a top-level table that will hold everything else.
-GameInfoX = {}
+if not GameInfoX then GameInfoX = {} end 
 
 GameInfoX.Addon = {
 	Name = "GameInfoX",
 	Title = "GameInfo eXtended",
-	Version = "1.0",
+	Version = "1.1",
 	Author = "@w33zl",
 	Description = ""
 }
@@ -27,35 +27,34 @@ GameInfoX.DefaultSettings = {
 	InventoryColor = "C5C29EFF",
 }
  
---[[
-GameInfoX.name = "GameInfoX"
-GameInfoX.version = "1.0"
-GameInfoX.description = ""
-GameInfoX.author = "@w33zl"
---]]
-
-
-
 function GameInfoX.MoveStop()
 end
 
-function GameInfoX.LootMessage(eventId, bagId, slotId, isNewItem, itemSoundCategory, updateReason)
-	-- if (GI.loaded == true) then
-	-- 	if(GI.vars.LootMsg==true) then
-	-- 		if (isNewItem==true) then
-	-- 			GI.QueuedLootindex=GI.QueuedLootindex+1
-	-- 			GI.QueuedLoot[GI.QueuedLootindex]=GI.strStrip(GetItemLink(bagId, slotId,LINK_STYLE_BRACKETS))
-	-- 		end
-	-- 	end
-	-- end
-end
+--[[function GameInfoX.LootMessage(eventId, bagId, slotId, isNewItem, itemSoundCategory, updateReason)
+	if (GI.loaded == true) then
+		if(GI.vars.LootMsg==true) then
+			d("Looot!")
+			--d("LootMsg EventID:" .. eventId .. ", isNewItem:" .. isNewItem .. ", updateReason:" .. updateReason)
+			d("event:" .. eventId)
+			d(isNewItem)
+			d("reason:" .. updateReason)
+			if (isNewItem==true) then
+				GI.QueuedLootindex=GI.QueuedLootindex+1
+				GI.QueuedLoot[GI.QueuedLootindex]=GI.strStrip(GetItemLink(bagId, slotId,LINK_STYLE_BRACKETS))
+			end
+		end
+	end
+end--]]
 
 function GameInfoX.WarnNumberAndColorizeText(text, number, warnTreshold, criticalTreshold)
 end
 
 function GameInfoX.Update()
+	
+
 	if (GI.loaded == true) then
-		if (GI.UpdateThrottle("UpdateX", 400) == true) then
+		if (GI.UpdateThrottle("UpdateX", 800) == true) then
+			GI.Update()
 			local usedSlots, maxSlots=PLAYER_INVENTORY:GetNumSlots(INVENTORY_BACKPACK)
 			local usedBankSlots, maxBankSlots2=PLAYER_INVENTORY:GetNumSlots(INVENTORY_BANK)
 			local bankIcon, maxBankSlots = GetBagInfo(BAG_BANK)
@@ -68,13 +67,52 @@ function GameInfoX.Update()
 				end
 				itemCounter = itemCounter + 1
 			end
-			GameInfoXDisplayBankCount:SetText(numberOfUsedBankSlots .." / ".. maxBankSlots)
+
+			local warnThreshold = 5
+			local defaultColor = "FFFFFF" --GI.vars.ColorLoot
+			
+			local bankColor
+			if numberOfUsedBankSlots == maxBankSlots then
+				--bankColor = "DD8C8C"
+				bankColor = "C08C8B"
+			elseif (maxBankSlots - numberOfUsedBankSlots ) <= warnThreshold then
+				--bankColor = "D9C66F"
+				bankColor = "B5AB7B"
+			end
+
+			local bagColor
+			if usedSlots == maxSlots then
+				--bagColor = "DD8C8C"
+				bagColor = "C08C8B"
+			elseif (maxSlots - usedSlots ) <= warnThreshold then
+				--bagColor = "D9C66F"
+				bagColor = "B5AB7B"
+			end
+
+			local bankSlotText
+			if bankColor == nil then
+				bankSlotText = numberOfUsedBankSlots
+			else
+				bankSlotText =  GI.ColorStart(bankColor) .. numberOfUsedBankSlots .. "|r"
+			end
+
+			local bagSlotText
+			if bagColor == nil then
+				bagSlotText = usedSlots
+			else
+				bagSlotText =  GI.ColorStart(bagColor) .. usedSlots .. "|r"
+			end
+
+			GameInfoXDisplayBankCount:SetText(bankSlotText .. " / ".. maxBankSlots)
 			--GameInfoXDisplayBankCount:SetText(usedBankSlotsLabel .." / ".. maxBankSlots .. " / " .. nUsed)
 			--GameInfoXDisplayBankCount:SetText("|cFFFFFF" .. usedBankSlotsLabel .." / ".. maxBankSlots .. "|r" .. " / " .. x)
+			GameInfoDisplayCount:SetText(bagSlotText .. " / ".. maxSlots)
 
 			GameInfoXDisplay:SetAlpha(0.5)
-			GameInfoDisplayBag:SetAlpha(0.5)
-			GameInfoDisplayCount:SetAlpha(0.5)
+			--GameInfoDisplayBag:SetAlpha(0.5)
+			--GameInfoXDisplayBankCount:SetAlpha(0.7)
+			--GameInfoDisplayCount:SetAlpha(0.7)
+			GameInfoDisplay:SetAlpha(0.5)
 
 		end
 	end
@@ -82,9 +120,13 @@ end
 
 -- Next we create a function that will initialize our addon
 function GameInfoX:Initialize()
+	-- Initialize bank icon
  	GameInfoXDisplayBank:SetTexture("ESOUI/art/icons/servicemappins/servicepin_bank.dds")
 
+ 	-- Override original update handler from GameInfo to be able to adjust some behaviors
+ 	GameInfoDisplay:SetHandler("OnUpdate", GameInfoX.Update)
 
+ 	-- We are all set!
 	d(GameInfoX.Addon.Name .. " " .. GameInfoX.Addon.Version .. " loaded!")
 end
  
@@ -100,3 +142,38 @@ end
 -- Finally, we'll register our event handler functions to be called when the proper events occurs.
 EVENT_MANAGER:RegisterForEvent(GameInfoX.name, EVENT_ADD_ON_LOADED, GameInfoX.OnAddOnLoaded)
 --EVENT_MANAGER:RegisterForEvent(GameInfoX.name, EVENT_INVENTORY_SINGLE_SLOT_UPDATE, GameInfoX.LootMessage)
+
+
+--[[
+
+EVENT_MANAGER:RegisterForEvent(wykkydsFramework.AddonID, EVENT_LOOT_RECEIVED, function(numID, lootedBy, itemName, quantity, itemSound, lootType, self)
+	if WF_SavedVars.LootNotice["Enabled"] then
+		if self then
+			d( "|c32DF41Loot Received: [ |r" .. itemName:gsub("%^%a+","") .. "|c32DF41 ] x " .. quantity .."|r" )
+		end
+	end
+end)
+EVENT_MANAGER:RegisterForEvent(wykkydsFramework.AddonID, EVENT_MONEY_UPDATE, function()
+	if WF_SavedVars.GoldNotice["Enabled"] then
+		local nowMoney = GetCurrentMoney()
+		if WF_SavedVars.GoldNotice["Scroll"] then
+			if nowMoney > wykkydsFramework.Money then
+				wykkydsFramework.ScrollFrames.Add( "+ " .. (nowMoney - wykkydsFramework.Money) .. " gold", "CENTER", {.5, 1, .5, 1} )
+			elseif wykkydsFramework.Money > nowMoney then
+				wykkydsFramework.ScrollFrames.Add( "- " .. (wykkydsFramework.Money - nowMoney) .. " gold", "CENTER", {1, .5, .5, 1} )
+			end
+		else
+			if nowMoney > wykkydsFramework.Money then
+				d( "|c32DF41Received Gold: " .. (nowMoney - wykkydsFramework.Money) .."|r" )
+			elseif wykkydsFramework.Money > nowMoney then
+				d( "|cDF3241Spent Gold: " .. (wykkydsFramework.Money - nowMoney) .."|r" )
+			end
+		end
+	end
+	wykkydsFramework.Money = GetCurrentMoney()
+end)
+
+
+--]]
+
+
