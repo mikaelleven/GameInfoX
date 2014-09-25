21a0d550e9f2595c4e163733a22e4541f708f0fb
mikaelleven <mikael.leven@gmail.com>
2014-09-25 13:33:35 +0200

    Added uncommited versions up to v1.6.1. Changes since 1.0: Fix for Update 4 Major refactoring Removed dependency of GameInfo(rmation) 3 The bag/bank window now properly hides when lock-picking, this should avoid any interference with lock-picking functionality Minor fixes to make it work with the 1.3.3 patch Added visual guidance (warning) for inventory slot status (BUGFIX) Fixed the issue with the Bag slot info disappearing when opening inventory

--- GameInfoX/GameInfoX.lua
+++ GameInfoX/GameInfoX.lua
@@ -1,5 +1,5 @@
 -------------------------------------------------------------------------------
--- Title: GameInfo eXtended													  -
+-- Title: w33zl's GameInfo (former 'GameInfo eXtended'						  -
 -- Author: @w33zl															  -
 -- Description: Extends and tweaks the behavior of the GameInfo addon		  -
 -------------------------------------------------------------------------------
@@ -9,13 +9,14 @@
 -- States and/or other countries. All rights reserved.						  -
 -------------------------------------------------------------------------------
 
+
 -- First, we create a namespace for our addon by declaring a top-level table that will hold everything else.
-GameInfoX = {}
+if not GameInfoX then GameInfoX = {} end 
 
 GameInfoX.Addon = {
 	Name = "GameInfoX",
-	Title = "GameInfo eXtended",
-	Version = "1.0",
+	Title = "w33zl's GameInfo",
+	Version = "1.6.1",
 	Author = "@w33zl",
 	Description = ""
 }
@@ -23,69 +24,96 @@ GameInfoX.Addon = {
 GameInfoX.DefaultSettings = {
 	SpaceInfo = true,
 	LockWindowPosition = false,
-	InventoryTransparency = 80,
+	InventoryTransparency = 50,
 	InventoryColor = "C5C29EFF",
+	PositionX = 20,
+	PositionY = 200
 }
  
---[[
-GameInfoX.name = "GameInfoX"
-GameInfoX.version = "1.0"
-GameInfoX.description = ""
-GameInfoX.author = "@w33zl"
---]]
-
-
-
 function GameInfoX.MoveStop()
-end
-
-function GameInfoX.LootMessage(eventId, bagId, slotId, isNewItem, itemSoundCategory, updateReason)
-	-- if (GI.loaded == true) then
-	-- 	if(GI.vars.LootMsg==true) then
-	-- 		if (isNewItem==true) then
-	-- 			GI.QueuedLootindex=GI.QueuedLootindex+1
-	-- 			GI.QueuedLoot[GI.QueuedLootindex]=GI.strStrip(GetItemLink(bagId, slotId,LINK_STYLE_BRACKETS))
-	-- 		end
-	-- 	end
-	-- end
+	GameInfoX.Settings.PositionX = GameInfoXDisplay:GetLeft()
+	GameInfoX.Settings.PositionY = GameInfoXDisplay:GetTop()
 end
 
 function GameInfoX.WarnNumberAndColorizeText(text, number, warnTreshold, criticalTreshold)
 end
 
-function GameInfoX.Update()
-	if (GI.loaded == true) then
-		if (GI.UpdateThrottle("UpdateX", 400) == true) then
-			local usedSlots, maxSlots=PLAYER_INVENTORY:GetNumSlots(INVENTORY_BACKPACK)
-			local usedBankSlots, maxBankSlots2=PLAYER_INVENTORY:GetNumSlots(INVENTORY_BANK)
-			local bankIcon, maxBankSlots = GetBagInfo(BAG_BANK)
-
-			local numberOfUsedBankSlots = 0
-			local itemCounter=0
-			while (itemCounter < maxBankSlots) do
-				if GetItemName(BAG_BANK, itemCounter) ~= "" then
-					numberOfUsedBankSlots = numberOfUsedBankSlots + 1
-				end
-				itemCounter = itemCounter + 1
-			end
-			GameInfoXDisplayBankCount:SetText(numberOfUsedBankSlots .." / ".. maxBankSlots)
-			--GameInfoXDisplayBankCount:SetText(usedBankSlotsLabel .." / ".. maxBankSlots .. " / " .. nUsed)
-			--GameInfoXDisplayBankCount:SetText("|cFFFFFF" .. usedBankSlotsLabel .." / ".. maxBankSlots .. "|r" .. " / " .. x)
-
-			GameInfoXDisplay:SetAlpha(0.5)
-			GameInfoDisplayBag:SetAlpha(0.5)
-			GameInfoDisplayCount:SetAlpha(0.5)
-
-		end
+function GameInfoX:HideUI()
+	if not GameInfoXDisplay:IsHidden() then
+		GameInfoXDisplay:SetHidden(true)
 	end
 end
 
--- Next we create a function that will initialize our addon
+function GameInfoX:ShowUI()
+	if GameInfoXDisplay:IsHidden() then
+		GameInfoXDisplay:SetHidden(false)
+	end
+end
+
+function GameInfoX:DoRefresh()
+	
+	if GameInfoX.ShouldBeHidden() then
+		GameInfoX:HideUI()
+		return
+	else
+		GameInfoX:ShowUI()
+	end
+
+	local usedSlots, maxSlots=PLAYER_INVENTORY:GetNumSlots(INVENTORY_BACKPACK)
+	local maxBankSlots = GetBagSize(BAG_BANK)
+	local numberOfUsedBankSlots = GetNumBagUsedSlots(BAG_BANK)
+
+	local warnThreshold = 5
+	local defaultColor = "FFFFFF" --GI.vars.ColorLoot
+	
+	local bankColor
+	if numberOfUsedBankSlots == maxBankSlots then
+		bankColor = "C08C8B"
+	elseif (maxBankSlots - numberOfUsedBankSlots ) <= warnThreshold then
+		bankColor = "B5AB7B"
+	end
+
+	local bagColor
+	if usedSlots == maxSlots then
+		bagColor = "C08C8B"
+	elseif (maxSlots - usedSlots ) <= warnThreshold then
+		bagColor = "B5AB7B"
+	end
+
+	local bankSlotText
+	if bankColor == nil then
+		bankSlotText = numberOfUsedBankSlots
+	else
+		bankSlotText =  "|c" .. bankColor .. numberOfUsedBankSlots .. "|r"
+	end
+
+	local bagSlotText
+	if bagColor == nil then
+		bagSlotText = usedSlots
+	else
+		bagSlotText =  "|c" .. bagColor .. usedSlots .. "|r"
+	end
+
+	GameInfoXDisplayBankCount:SetText(bankSlotText .. " / ".. maxBankSlots)
+	GameInfoXDisplayCount:SetText(bagSlotText .. " / ".. maxSlots)
+
+	GameInfoXDisplay:SetAlpha(0.5)
+end
+
 function GameInfoX:Initialize()
- 	GameInfoXDisplayBank:SetTexture("ESOUI/art/icons/servicemappins/servicepin_bank.dds")
 
+	GameInfoX.Settings = ZO_SavedVars:New("GIX_Config", 1, nil, GameInfoX.DefaultSettings)
 
+	if GameInfoX.Settings.PositionX and GameInfoX.Settings.PositionY then
+		GameInfoXDisplay:ClearAnchors()
+		GameInfoXDisplay:SetAnchor(TOPLEFT, GuiRoot, TOPLEFT, GameInfoX.Settings.PositionX, GameInfoX.Settings.PositionY)
+		GameInfoXDisplay:SetMovable(true)
+	end
+
+ 	-- We are all set!
 	d(GameInfoX.Addon.Name .. " " .. GameInfoX.Addon.Version .. " loaded!")
+
+	EVENT_MANAGER:RegisterForUpdate("GameInfoX_Update",500,function() GameInfoX:DoRefresh() end)
 end
  
 -- Then we create an event handler function which will be called when the "addon loaded" event
@@ -96,7 +124,28 @@ function GameInfoX.OnAddOnLoaded(event, addonName)
     GameInfoX:Initialize()
   end
 end
+
+--- Check to see if lockpicking window is enabled
+function GameInfoX.ShouldBeHidden()
+	if not LOCK_PICK["control"]:IsHidden() then return true end
+	return false
+end
+
+function GameInfoX.OnGuiHidden(eventCode, guiName, hidden)
+	--d("Gui hidden")
+end
+
  
 -- Finally, we'll register our event handler functions to be called when the proper events occurs.
 EVENT_MANAGER:RegisterForEvent(GameInfoX.name, EVENT_ADD_ON_LOADED, GameInfoX.OnAddOnLoaded)
---EVENT_MANAGER:RegisterForEvent(GameInfoX.name, EVENT_INVENTORY_SINGLE_SLOT_UPDATE, GameInfoX.LootMessage)
+-- EVENT_MANAGER:RegisterForEvent(GameInfoX.name, EVENT_GUI_HIDDEN, GameInfoX.OnGuiHidden)
+ 
+
+-- --EVENT_MANAGER:RegisterForEvent(GameInfoX.name, EVENT_INVENTORY_SINGLE_SLOT_UPDATE, GameInfoX.LootMessage)
+-- EVENT_MANAGER:RegisterForEvent(GameInfoX.name, EVENT_BEGIN_LOCKPICK, function() 
+-- 	--d("Begin lockpick")
+-- end)
+
+-- Register slash commands
+SLASH_COMMANDS["/rl"] = function() ReloadUI("ingame") end
+SLASH_COMMANDS["/mem"] = function() d(math.ceil(collectgarbage("count")).." KB") end
--- GameInfoX/readme.md
+++ GameInfoX/readme.md
@@ -1,12 +1,14 @@
-# GameInfo eXtended (TESO AddOn)
+# GameInfo eXtended (TESO AddOn) by @w33zl
 
 GameInfo eXtended is a tweak on top of the _GameInfo_ addon by _@Sternentau_. Initially the purpose was to simply add bank slot status in addition to the inventory slot status provided by _GameInfo_. However, along the way some additional tweaks was added.
 
 ## Main features
-- Displays bank info (used and total available slots)
-    + Docks into the Bag information from _GameInfo_
-- Disables bag info auto-hiding from _GameInfo_ (when opening the Bank for example)
+- Displays quick summary of total and used bank slots
+    + Docks into the inventory slot information from _GameInfo_
+- Disables bag info auto-hiding from _GameInfo_ when opening the Bank or Inventory
 - Adds alpha-channel (transparency) to the Bag and Bank information to give it a more subtle look (to limit the interference with the game immersion).
+- Visual guidance for inventory and bank slot status, yellow when nearly full (warning) and red when completely full (critical).
+- Slash commands for LUA memory usage (/mem) and to reload ui (/rl)
 
 ## Planned / requested features
 - Automatically increase opacity of Bag and Bank info when opening the inventory and or the bank
@@ -15,7 +17,9 @@ GameInfo eXtended is a tweak on top of the _GameInfo_ addon by _@Sternentau_. In
 - Tweak Loot info with colors, alpha-channel and stack info
 - Add XP info at exp-bar, auto-hide text after n secs
 - Change visual style of gold loot
-- Add visual guidance (warning) for bag/bank status (full etc)
+
+## Version history
+See TODO document for revisions made.
 
 ## Disclaimer
 This Add-on is not created by, affiliated with or sponsored by ZeniMax Media Inc. or its affiliates. The Elder Scrolls® and related logos are registered trademarks or trademarks of ZeniMax Media Inc. in the United States and/or other countries. All rights reserved.
